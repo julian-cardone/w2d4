1c22395aa0ca50836adefecbbbaf4ee256d8503e
Julian Cardone <juliancardone1@gmail.com>
2022-09-22 15:18:39 -0400

    finished char_indeces

---
+++ recap_exercise_3/char_indices.rb
@@ -0,0 +1,14 @@
+def char_indices(string)
+
+    hash = Hash.new{|hash, key| hash[key] = []}
+
+    (0...string.length).each {|i| hash[string[i]] << i}
+
+    hash
+
+
+end
+
+# Examples
+p char_indices('mississippi')   # => {"m"=>[0], "i"=>[1, 4, 7, 10], "s"=>[2, 3, 5, 6], "p"=>[8, 9]}
+p char_indices('classroom')     # => {"c"=>[0], "l"=>[1], "a"=>[2], "s"=>[3, 4], "r"=>[5], "o"=>[6, 7], "m"=>[8]}
