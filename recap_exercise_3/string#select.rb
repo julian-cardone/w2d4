512df668a9381c5dd58339d7d975ed6969d33877
Julian Cardone <juliancardone1@gmail.com>
2022-09-22 20:18:51 -0400

    started class string select

---
+++ recap_exercise_3/string#select.rb
@@ -0,0 +1,10 @@
+class String
+
+
+
+end
+
+# Examples
+p "app academy".select { |ch| !"aeiou".include?(ch) }   # => "pp cdmy"
+p "HELLOworld".select { |ch| ch == ch.upcase }          # => "HELLO"
+p "HELLOworld".select                                   # => ""
